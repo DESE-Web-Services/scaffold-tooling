25e3cb87a8be715a91c28f597bdc272bc1c721f1
Toby Harper <Toby.Harper@dese.gov.au>
2020-11-06 09:00:54 +1100

    add config overrides to disable akamai purging
    modules will remain enabled but calls to akamai are disabled and dummy key values are set to prevent errors from missing environment variables
    
    Signed-off-by: Toby Harper <Toby.Harper@dese.gov.au>

--- drupal/settings/development.settings.php
+++ drupal/settings/development.settings.php
@@ -38,6 +38,21 @@ $config['google_analytics.settings']['account'] = 'UA-XXXXXXXX-YY';
 $config['system.performance']['cache']['page']['max_age'] = 0;
 
 /**
+ * Disable Akamai purging.
+ */
+$config['akamai.settings']['disabled'] = TRUE;
+
+/**
+ * Set dummy Key values in place of missing env variables.
+ */
+$config['key.key.akamai_access_token']['key_provider'] = 'config';
+$config['key.key.akamai_access_token']['key_provider_settings']['key_value'] = '1';
+$config['key.key.akamai_client_token']['key_provider'] = 'config';
+$config['key.key.akamai_client_token']['key_provider_settings']['key_value'] = '1';
+$config['key.key.akamai_client_secret']['key_provider'] = 'config';
+$config['key.key.akamai_client_secret']['key_provider_settings']['key_value'] = '1';
+
+/**
  * Disable CSS and JS aggregation.
  */
 $config['system.performance']['css']['preprocess'] = FALSE;
